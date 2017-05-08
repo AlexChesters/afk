63bc8f104569e04f179df2e50d0d573aa83241c1
Alex Chesters <alex.chesters@bbc.co.uk>
2017-05-08 16:17:01 +0100

    Remove uneeded code

--- src/face_detector.py
+++ src/face_detector.py
@@ -1,16 +1,6 @@
-import dlib
-import base64
-import time
-from skimage import io
 import boto3
 
 client = boto3.client('rekognition', region_name='eu-west-1')
-
-def count_faces(image_path):
-    img = io.imread(image_path)
-    detector = dlib.get_frontal_face_detector()
-    dets = detector(img, 1)
-    return len(dets)
 
 def compare_faces(source_image_path, target_image_path):
     source_image = open(source_image_path, 'rb')

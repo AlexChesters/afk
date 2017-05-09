f2d633535d48e0a7cd03fb84eca23de06877cf2f
Alex Chesters <AlexChesters@users.noreply.github.com>
2017-05-09 10:47:19 +0100

    Update take_picture.py

--- src/take_picture.py
+++ src/take_picture.py
@@ -2,7 +2,8 @@ from subprocess import call
 from os import devnull
 
 DEVNULL = open(devnull, 'w')
+IMAGE_PATH = "image.png"
 
 def take_picture():
-    call(["imagesnap", "-w", "1", "image.png"], stdout=DEVNULL)
-    return 'image.png'
+    call(["imagesnap", "-w", "1", IMAGE_PATH], stdout=DEVNULL)
+    return IMAGE_PATH
